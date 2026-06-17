b5cdc59a62d8970ace21460c1152be0ee066fc7e
Rusiru <liyanagerusiru12@gmail.com>
2026-06-17 18:37:12 +0530

    SCRUM-34 Create UserService and email uniqueness check

---
+++ backend/src/main/java/com/hospital/backend/service/UserService.java
@@ -0,0 +1,24 @@
+package com.hospital.backend.service;
+
+import com.hospital.backend.entity.User;
+import com.hospital.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+        return userRepository.save(user);
+    }
+}
